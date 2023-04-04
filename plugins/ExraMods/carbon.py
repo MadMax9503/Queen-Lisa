8d16ff78c2f90bc2cb71123806a0f302ddaa13d7
MadMax9503 <111670816+MadMax9503@users.noreply.github.com>
2023-04-04 17:51:04 +0530

    Update carbon.py

--- plugins/ExraMods/carbon.py
+++ plugins/ExraMods/carbon.py
@@ -42,8 +42,8 @@ async def carbon_func(_, message):
     await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
     await message.reply_photo(
         photo=carbon,
-        caption="**Made by @mkn_bots_updates**",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝚄𝚂", url="https://t.me/mkn_bots_updates")]]),                   
+        caption="**Made by @Network10xOfficial**",
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ꜰʟᴀꜱʜ ᴍᴏᴠɪᴇꜱ", url="https://t.me/FlashTenXMovies")]]),                   
     )
     await m.delete()
     carbon.close()
