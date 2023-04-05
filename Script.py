18b02bb30d87c2c56f8bbe1b5c4c672aab4b47d5
Uniqueboy10x <111670816+MadMax9503@users.noreply.github.com>
2023-04-05 18:30:51 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -72,7 +72,7 @@ class script(object):
 ›› /set_template - 𝚂𝙴𝚃 𝙲𝚄𝚂𝚃𝙾𝙼 𝙸𝙼𝙳𝙱 𝚃𝙴𝙼𝙿𝙻𝙰𝚃𝙴 𝙵𝙾𝚁 𝙰𝚄𝚃𝙾 𝙵𝙸𝙻𝚃𝙴𝚁. 
 ›› /get_template - 𝙶𝙴𝚃 𝙲𝚄𝚁𝚁𝙴𝙽𝚃 𝙸𝙼𝙳𝙱 𝚃𝙴𝙼𝙿𝙻𝙰𝚃𝙴 𝙾𝙵 𝙰𝚄𝚃𝙾 𝙵𝙸𝙻𝚃𝙴𝚁.
 
-𝙲𝚁𝙴𝙳𝙸𝚃𝚂 :- <a href=https://t.me/mr_MKN>Mr.MKN TG</a>**"""
+𝙲𝚁𝙴𝙳𝙸𝚃𝚂 :- <a href=https://t.me/Network10xOfficial>𝙽𝙴𝚃𝚆𝙾𝚁𝙺𝟷𝟶𝚇𝙾𝙵𝙵𝙸𝙲𝙸𝙰𝙻</a>**"""
 
     CONNECTION_TXT = """Help: <b>Connections</b>
 
@@ -184,7 +184,7 @@ By @{} """
 
 ➪ 𝖩𝗎𝗌𝗍 𝗌𝖾𝗇𝖽 𝗆𝖾 𝖺 𝗂𝗆𝖺𝗀𝖾 𝗍𝗈 𝖾𝖽𝗂𝗍 ✨
 
-𝖬𝖺𝖽𝖾 𝖻𝗒 <a href=https://t.me/mr_MKN>Mr.MKN TG</a>"""
+𝖬𝖺𝖽𝖾 𝖻𝗒 <a href=https://t.me/Network10xOfficial>𝙽𝙴𝚃𝚆𝙾𝚁𝙺𝟷𝟶𝚇𝙾𝙵𝙵𝙸𝙲𝙸𝙰𝙻</a>"""
 
     RESTRIC_TXT = """➤ 𝐇𝐞𝐥𝐩: Mᴜᴛᴇ 🚫
 
