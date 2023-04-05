58cb25e4f6f386ac42b51acfa27c564b791e56b0
Uniqueboy10x <111670816+MadMax9503@users.noreply.github.com>
2023-04-05 20:02:29 +0530

    Update telegraph.py

--- plugins/ExraMods/telegraph.py
+++ plugins/ExraMods/telegraph.py
@@ -31,11 +31,13 @@ async def telegraph_upload(bot, update):
         print(error)
         return    
     await text.edit_text(
-        text=f"<b>Link :-</b>\n\n<code>https://graph.org{response[0]}</code>",
+        text=f"<b>Link :-</b>\n\n<code>https://graph.org{response[0]}</code>\n\n<b>𝙼𝙰𝙳𝙴 𝚆𝙸𝚃𝙷 ❤ 𝙱𝚈 [𝙽𝙴𝚃𝚆𝙾𝚁𝙺𝟷𝟶𝚇 𝙾𝙵𝙵𝙸𝙲𝙸𝙰𝙻](https://t.me/Network10xOfficial</b>",
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup( [[
             InlineKeyboardButton(text="Open Link", url=f"https://graph.org{response[0]}"),
             InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url=https://graph.org{response[0]}")
+            [
+            InlineKeyboardButton(text="𝑭𝒍𝒂𝒔𝒉 𝑴𝒐𝒗𝒊𝒆𝒔", url=f"https://t.me/FlashTenXMovies")
             ],[
             InlineKeyboardButton(text="✗ Close ✗", callback_data="close")
             ]])
